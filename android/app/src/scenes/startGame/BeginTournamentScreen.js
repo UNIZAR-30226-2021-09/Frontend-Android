258642d66b9cf94c545eb0489746dbb26a1c580a
SergioGabete <81410994+SergioGabete@users.noreply.github.com>
2021-05-18 16:30:15 +0200

    Se ha añadido en lo de torneos que se elijan 3 amigos

--- android/app/src/scenes/startGame/BeginTournamentScreen.js
+++ android/app/src/scenes/startGame/BeginTournamentScreen.js
@@ -1,26 +1,63 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, TouchableWithoutFeedback, StyleSheet, Image, Alert } from 'react-native';
+import { View, Text, TouchableHighlight, TouchableWithoutFeedback, StyleSheet, Image, Alert,  AsyncStorage} from 'react-native';
 import { PRIMARY, SECONDARY, BLACK } from '../../styles/colors';
 import { BarraLateral } from '_organisms'
 import DropDownPicker from 'react-native-dropdown-picker';
+import { getFriendList } from '_api/user';
+import {Picker} from '@react-native-picker/picker';
+
 
 export default class BeginTournamentScreen extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tourList: [
-                { label: 'Torneo 1', value: 'torneo1', },
-                { label: 'Torneo 2', value: 'torneo2' },
-                { label: 'Torneo 3', value: 'torneo3' },
-            ],
-            selectedTour: null
+            Amigo1: "",
+            TokenAmigo1: "",
+            Amigo2: "",
+            TokenAmigo2: "",
+            Amigo3: "",
+            TokenAmigo3: "",
+            listaAmigos: [],
+            //listaAmigosDDP: [{label:"", nombre:""}],
         }
     }
+    /*async crearLabels(){
+        for(const elemento in this.state.listaAmigos){
+            this.state.listaAmigosDDP.push({label: elemento,nombre: elemento});
+            console.log("Nueva lista cread: " + this.state.listaAmigosDDP);
+        }
+    }*/
+    
 
-    render() {
-        return (<View style={styles.container}>
-            <View style={styles.cuadroGrande}>
-                <Text style={styles.title} > Torneo</Text>
+
+    async componentDidMount() {
+        var _username = await AsyncStorage.getItem('username');
+        var accessToken = await AsyncStorage.getItem('userToken');
+        var user = {
+            Username: _username,
+            AccessToken: accessToken
+        };
+        console.log(user);
+        await getFriendList(user).then(data => {
+            console.log("Data de getFriendList: " + data);
+            if (data != "error") {
+                this.setState(
+                    { listaAmigos: data }
+                )
+                this.crearLabels();
+            } else {
+                alert('Error de getFriendList');
+            }
+        }).catch(err => {
+            console.log("error getFriendList")
+            console.log(err)
+            return "error"
+        });        
+    }
+
+    
+/*
+<Text style={styles.title} > Torneo</Text>
                 <DropDownPicker
                     items={this.state.tourList}
                     defaultValue={this.state.selectedTour}
@@ -33,12 +70,79 @@ export default class BeginTournamentScreen extends Component {
                     placeholderStyle={styles.text}
                 />
             </View>
+*/
+    render() {
+        return (<View style={styles.container}>
+            <View style={styles.cuadroGrande}>
+            <Text style={styles.title}>
+                Selecciona 3 amigos
+            </Text>
+            
+            <Picker
+                style={styles.dp1}
+                labelStyle={styles.listText}
+                placeholderStyle={styles.text}
+                mode="dropdown"
+                selectedValue={this.state.Amigo1}
+                itemStyle={{ backgroundColor: "grey", color: "blue", fontFamily:"Ebrima", fontSize:17 }}
+                onValueChange={()=>{}}> 
+                {this.state.listaAmigos.map((item, index) => {
+                    return (<Picker.Item label={item} value={index} key={index}/>) 
+                })}
+                
+            </Picker>
+            
+            <Picker
+                style={styles.dp2}
+                mode="dropdown"
+                selectedValue={this.state.Amigo2}
+                onValueChange={()=>{}}> 
+                {this.state.listaAmigos.map((item, index) => {
+                    return (<Picker.Item label={item} value={index} key={index}/>) 
+                })}
+            </Picker>
+            
+            <Picker
+                style={styles.dp3}
+                mode="dropdown"
+                selectedValue={this.state.Amigo2}
+                onValueChange={()=>{}}> 
+                {this.state.listaAmigos.map((item, index) => {
+                    return (<Picker.Item label={item} value={index} key={index}/>) 
+                })}
+            </Picker>
+
+            </View>
             <BarraLateral navigation={this.props.navigation} />
 
         </View>);
     }
 };
 const styles = StyleSheet.create({
+    dp1:{
+        
+        height: 50, width: 260, alignSelf: 'center',
+        paddingTop: 20,
+        paddingBottom: 20,
+        
+       
+    },
+    dp2:{
+        
+        height: 100, width: 260, alignSelf: 'center',
+        paddingTop: 20,
+        paddingBottom: 20,
+
+    },
+    dp3:{
+        height: 60, width: 260, alignSelf: 'center',
+        paddingTop: 20,
+        paddingBottom: 20,
+        borderColor: 'red', borderRadius: 4,
+        
+    },
+    
+
     container: {
         flex: 1,
         flexDirection: 'row',
