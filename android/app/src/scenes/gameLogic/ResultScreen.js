49afb0625e94e77efcf9a067135dd6710c0a8c83
Binhui Chen Zhou <62297968+779799@users.noreply.github.com>
2021-05-21 21:21:24 +0200

    Pantalla de resultados

--- android/app/src/scenes/gameLogic/ResultScreen.js
+++ android/app/src/scenes/gameLogic/ResultScreen.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, TouchableWithoutFeedback, StyleSheet, Image, Alert } from 'react-native';
+import { View, Text, TouchableHighlight, TouchableWithoutFeedback, StyleSheet, Image, Alert, AsyncStorage } from 'react-native';
 import { PRIMARY, SECONDARY, BLACK } from '../../styles/colors';
 import { BarraLateral } from '_organisms'
-import { checkBox, OCEAN_BOX, TOUCHED_BOX, SUNKEN_BOX, NO_ATACK_BOX } from '_api/gameLogic';
+import { checkBox, OCEAN_BOX, TOUCHED_BOX, SUNKEN_BOX, NO_ATACK_BOX } from '_api/match';
 import { Table, TableWrapper, Cell } from 'react-native-table-component'
-import { initBoard, getBox, attack, getSolution, initAttack, initShip, initCoord, IAmove, getCoord } from '../../api/gameLogic';
+import { infoPartida } from '_api/match';
 export default class ResultScreen extends Component {
     constructor(props) {
         super(props);
-        let result = this.props.navigation.getParam("result");
         this.state = {
-            myBoard: {
-                victory: result
-            },
-            IABoard: {
-                victory: !result
-            },
+            ganador: false,
+            puntos: 0,
+            disparosRealizados: 0,
+            disparosAcertados: 0,
+            barcosDestruidos:0
         }
     }
+    async componentDidMount() {
+        var username = await AsyncStorage.getItem('username');
+        var accessToken = await AsyncStorage.getItem('userToken');
+        var gameId = await AsyncStorage.getItem('gameId');
+
+        await infoPartida(username, accessToken, gameId)
+            .then(data => {
+                console.log("Data de infoPartida: " + JSON.stringify(data));
+                this.setState({
+                    ganador: data.ganador,
+                    puntos: data.puntos,
+                    disparosRealizados: data.disparosRealizados,
+                    disparosAcertados: data.disparosAcertados,
+                    barcosDestruidos: data.barcosDestruidos,
+                })
+            })
+    }
+     
+
+    
     render() {
+        let { ganador, puntos, disparosAcertados, disparosRealizados, barcosDestruidos } = this.state
         return (
             <View style={styles.container}>
                 <View style={styles.cuadroGrande}>
                     <View style={styles.cuadroPequeno}>
-                        <Text style={styles.title} > Resultados </Text>
+                        <Text style={styles.title} > {ganador ? "Has ganado " : "Has perdido "}la partida</Text>
+                    </View>
+                    <View style={styles.estadisticas}>
+                        <Text style={styles.text} > Puntos obtenidos: {puntos}</Text>
+                        <Text style={styles.text} > Disparos realizados: {disparosRealizados} </Text>
+                        <Text style={styles.text} > Disparos acertados: {disparosAcertados}</Text>
+                        <Text style={styles.text} > Barcos destruidos: {barcosDestruidos}</Text>
                     </View>
                     <View style={styles.cuadroPequeno}>
-                        <Text style={styles.text} > Has ganado la partida contra IA</Text>
+                        <TouchableHighlight onPress={() => this.props.navigation.navigate('Home')}>
+                            <View style={styles.button}>
+                                <Text style={styles.btnText}> Terminar </Text>
+                            </View>
+                        </TouchableHighlight>
                     </View>
-                    <TouchableHighlight onPress={() => this.props.navigation.navigate('Home')}>
-                        <View style={styles.button}>
-                            <Text style={styles.btnText}> Terminar </Text>
-                        </View>
-                    </TouchableHighlight>
                 </View>
 
                 <BarraLateral navigation={this.props.navigation} />
@@ -59,16 +83,22 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignSelf: 'center'
     },
+    estadisticas: {
+        flex: 2,
+        borderColor: BLACK,
+        flexDirection: 'column',
+        alignSelf: 'center'
+    },
     title: {
         textAlign: 'center',
         color: 'black',
-        paddingTop: 20,
+        paddingTop: 16,
         paddingBottom: 20,
-        fontSize: 38,
+        fontSize: 34,
         fontWeight: 'bold',
     },
     text: {
-        fontSize: 20,
+        fontSize: 24,
     },
     listContainer: {
         height: 50, width: 260, alignSelf: 'center'
@@ -76,8 +106,8 @@ const styles = StyleSheet.create({
     listText: {
         fontSize: 18, justifyContent: 'center', color: 'black'
     },
-    button: { alignSelf: 'center', width: 100, height: 30, bottom: 15, backgroundColor: PRIMARY, borderRadius: 50, borderWidth: 0.2 },
-    btnText: { textAlign: 'center', color: 'white', paddingTop: 5 },
+    button: { alignSelf: 'center', width: 110, height: 40, backgroundColor: PRIMARY, borderRadius: 50, borderWidth: 0.2 },
+    btnText: { textAlign: 'center', color: 'white', paddingTop: 5, fontSize:18 },
 
 });
 
